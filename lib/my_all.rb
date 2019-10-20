93922574b5ae4a70f27b2f2d9d19ffdde06dcea9
Tavis Weeden <tavisweeden@gmail.com>
2019-10-20 02:06:53 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,8 +2,15 @@ require 'pry'
 
 def my_all?(collection)
   i = 0
+  block_return_values = []
   while i < collection.length
     i = i + 1  
-  binding.pry
+  block_return_values << yield collection([i])
+  end
+  if 
+  block_return_values.include?(false)
+    false
+  else
+    true
   end
 end
